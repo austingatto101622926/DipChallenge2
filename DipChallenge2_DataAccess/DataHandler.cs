53e691cd158b51e205a8d4748f97a18fb9cb6ab3
f.,mng <kbjgfkj>
2018-06-22 12:31:55 +1000

    DataHandler

---
+++ DipChallenge2_DataAccess/DataHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DipChallenge2_DataAccess
+{
+    public class DataHandler
+    {
+        public EventHandler Event;
+
+        public DataHandler(string baseAddress)
+        {
+            Event = new EventHandler(baseAddress);
+        }
+    }
+}
---
+++ DipChallenge2_DataAccess/DataHandlers/EventHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace DipChallenge2_DataAccess
+{
+    public class EventHandler
+    {
+        HttpClient Client;
+        public EventHandler(string baseAddress)
+        {
+            Client = new HttpClient() { BaseAddress = new Uri(baseAddress) };
+        }
+        
+
+    }
+}
